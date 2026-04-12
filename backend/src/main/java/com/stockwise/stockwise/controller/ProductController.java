89b5065f4b3d2a91e1a26caa7b98d7a7e6283b9a
pl23022 <pl23022@ues.edu.sv>
2026-04-12 13:18:07 -0700

    segundo intento productControler

--- backend/src/main/java/com/stockwise/stockwise/controller/ProductController.java
+++ backend/src/main/java/com/stockwise/stockwise/controller/ProductController.java
@@ -6,43 +6,43 @@ import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
-import com.stockwise.stockwise.service.ProductService;
-import com.stockwise.stockwise.dto.ProductRequestDTO;
-import com.stockwise.stockwise.dto.ProductResponseDTO;
+import com.stockwise.stockwise.service.productService;
+import com.stockwise.stockwise.dto.productRequestDTO;
+import com.stockwise.stockwise.dto.productResponseDTO;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final ProductService service;
+    private final productService service;
 
-    public ProductController(ProductService service) {
+    public ProductController(productService service) {
         this.service = service;
     }
 
     // GET ALL
     @GetMapping
-    public ResponseEntity<List<ProductResponseDTO>> getAll() {
+    public ResponseEntity<List<productResponseDTO>> getAll() {
         return ResponseEntity.ok(service.getAll());
     }
 
     // GET BY ID
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> getById(@PathVariable Long id) {
+    public ResponseEntity<productResponseDTO> getById(@PathVariable Long id) {
         return ResponseEntity.ok(service.getById(id));
     }
 
     // POST
     @PostMapping
-    public ResponseEntity<ProductResponseDTO> create(@RequestBody ProductRequestDTO product) {
+    public ResponseEntity<productResponseDTO> create(@RequestBody productRequestDTO product) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.create(product));
     }
 
     // PUT
     @PutMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> update(@PathVariable Long id,
-                                                     @RequestBody ProductRequestDTO product) {
+    public ResponseEntity<productResponseDTO> update(@PathVariable Long id,
+                                                     @RequestBody productRequestDTO product) {
         return ResponseEntity.ok(service.update(id, product));
     }
 
